0a8fbfab6b5dfa581a5d62d66cf265b82b87cb68
Danial Faizsal <m.d.faizsal@gmail.com>
2021-07-12 03:06:22 +0800

    Grid cells on mouseover will change to a random colour

--- js/script.js
+++ js/script.js
@@ -6,8 +6,19 @@ function makeRows(rows, cols) {
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
       cell.innerText = (c + 1);
+      cell.addEventListener("mouseover", changeGridColor);
       container.appendChild(cell).className = "grid-item";
     };
   };
+
+function changeGridColor(event) {
+  let a = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let c = Math.floor(Math.random() * 256);
+  event.target.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+  event.target.style.opacity += 0.1;
+
+  console.log(event)
+}
 
 makeRows(16, 16);
